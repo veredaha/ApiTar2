71a40a9e9eae65a51f650ccad5edbd0589243112
veredaha <97604250+veredaha@users.noreply.github.com>
2022-08-06 20:54:49 +0300

    collection ob isbn

--- models/collectionofIsbn.py
+++ models/collectionofIsbn.py
@@ -6,10 +6,26 @@ class CollectionOfIsbn:
         self._isbn = isbn
 
 class AddListOfBooks(CollectionOfIsbn):
-    def __init__(self,userId:str,collectionOfIsbns:CollectionOfIsbn) -> None:
+    def __init__(self,userId:str,collectionOfIsbns:None) -> None:
         """function creates class AddListOfBooks
          :returns: None 
          """
         self._userId=userId
-        self._collectionOfIsbns =collectionOfIsbns
+        self._collectionOfIsbns =None
+        if collectionOfIsbns is not None:
+            self.collectionOfIsbns =collectionOfIsbns
+    def toJson(self) -> str:
+        """from class to json
+         :returns: str
+         """
+        result = {}
+        for key, val  in self.__dict__.items():
+            if val is not None:
+                if key.startswith("_"):
+                    result[key[1:]] = val
+                else:
+                    result[key] = val
+        return result
+
+
 
