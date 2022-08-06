af3ba5667fd97214e5d9d55f1582717a74a1846f
veredaha <97604250+veredaha@users.noreply.github.com>
2022-08-06 20:52:30 +0300

    user book results

--- models/userbooksresult.py
+++ models/userbooksresult.py
@@ -1,26 +1,69 @@
 class UserBooksResult: 
  def __init__(self, userId:str, isbn:str, message:str) -> None:
+    """function creates class UserBooksResult
+       :returns: None 
+    """
     self._userId =userId	  
     self._isbn = isbn
     self._message = message
 
 class MessageModal:
+    
     def __init__(self,code:int, message:str) -> None:
+       """function creates class MessageModal
+       :returns: None 
+          """
        self._code = code
        self._message = message
 
 class ReplaceIsbn:
+
     def __init__(self, userId:str, isbn:str) -> None:
+       """function creates class ReplaceIsbn
+         :returns: None 
+       """
        self._userId =userId	  
        self._isbn = isbn
+    def toJson(self) -> str:
+        """from class to json
+         :returns: str
+         """
+        result = {}
+        for key, val  in self.__dict__.items():
+            if val is not None:
+                if key.startswith("_"):
+                    result[key[1:]] = val
+                else:
+                    result[key] = val
+        return result
 
 class BooksResult:
+    
     def __init__(self,userId:str, message:str) -> None:
+       """function creates class BooksResult
+       :returns: None 
+         """
        self._userId =userId	
        self._message = message
 
 
 class StringObject:
     def __init__(self, isbn:str, userId:str) -> None:
+       """function creates class StringObject
+       :returns: None 
+        """
        self._isbn = isbn
        self._userId =userId	 
+
+    def toJson(self) -> str:
+        """from class to json
+         :returns: str
+         """
+        result = {}
+        for key, val  in self.__dict__.items():
+            if val is not None:
+                if key.startswith("_"):
+                    result[key[1:]] = val
+                else:
+                    result[key] = val
+        return result
