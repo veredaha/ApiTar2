0f79c4405cda6b35005c104147b8cc1c55954c27
veredaha <97604250+veredaha@users.noreply.github.com>
2022-08-06 23:23:52 +0300

    pet

--- petstore/models/pet.py
+++ petstore/models/pet.py
@@ -1,10 +1,26 @@
-import json
+
 from enum import Enum
 
-class baseObj:
 
-    def __init__(self):
-        pass
+class Pet():
+
+    def __init__(self, id, name, category=None, photoUrls=None, tags=None, status=None)-> None:
+        """function creates class Pet
+         :returns: None 
+         """
+        self._photo_urls = None
+        self._tags = None
+        self._status = None
+        self._id = id
+        self._name = name
+        self._category = category
+        if photoUrls is not None:
+            self._photo_urls = photoUrls
+        if tags is not None:
+            self._tags = tags
+        if status is not None:
+            Status[status]
+            self._status = status
 
     def toJson(self) -> str:
         """from class to json
@@ -20,34 +36,16 @@ class baseObj:
         return result
 
 
-    def __str__(self):
-        return json.dumps(self.toJson())
-
-class Pet(baseObj):
-
-    def __init__(self, id, name, category=None, photoUrls=None, tags=None, status=None)-> None:
-        self._photo_urls = None
-        self._tags = None
-        self._status = None
-        self._id = id
-        self._name = name
-        self._category = category
-        if photoUrls is not None:
-            self._photo_urls = photoUrls
-        if tags is not None:
-            self._tags = tags
-        if status is not None:
-            Status[status]
-            self._status = status
-
-
 class Status(Enum):
     available = "available"
     pending = "pending"
     sold = "sold"
 
-class Tag(baseObj):
+class Tag():
     def __init__(self,id = None, name = None) -> None:
+        """function creates class Tag
+         :returns: None 
+         """
         self._id = None
         self._name = None
         if id is not None:
@@ -55,9 +53,25 @@ class Tag(baseObj):
         if name is not None:
             self._name = name
 
-class Category(baseObj):
+    def toJson(self) -> str:
+        """from class to json
+         :returns: str
+         """
+        result = {}
+        for key, val  in self.__dict__.items():
+            if val is not None:
+                if key.startswith("_"):
+                    result[key[1:]] = val
+                else:
+                    result[key] = val
+        return result
+
+class Category():
 
     def __init__(self, id=None, name=None):
+        """function creates class Category
+         :returns: None 
+         """
 
         self._id = None
         self._name = None
