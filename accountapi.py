c238f0c34a6d17870871a552eb060b34b9b9e072
veredaha <97604250+veredaha@users.noreply.github.com>
2022-08-06 20:49:34 +0300

    accountapi.py

--- accountapi.py
+++ accountapi.py
@@ -1,10 +1,9 @@
 import json
 import requests
 import pytest  
-from models.tokenviewmodel import LoginViewModel
-from models.tokenviewmodel import TokenViewModel
-from models.bookmodel import BookModal
-from models.bookmodel import GetUserResult
+from models.tokenviewmodel import LoginViewModel,TokenViewModel
+from models.bookmodel import BookModal, CreateUserResult,GetUserResult
+from models.userbooksresult import MessageModal
 
 
 
@@ -26,82 +25,90 @@ def bearer_auth_session()->requests:
 
 class accountApi():
 
-    def __init__(self, url : str,headrs:str)-> None:
+    def __init__(self, url : str, headers:str)-> None:
         """function creates class accountApi
          :returns: None 
          """
-        self.url = url
-        self.headrs = headrs
-        self.session = requests.session()
-        self.session.headers.update(self.headrs)
-   
-    def post_account_authorize(self, account:LoginViewModel)->None:
-        """ post account
-     :returns: LoginViewModel
-     """
-        account_data = account.toJson()
-        res = self.session.post(url=f"{self.url}/account/v1/AuthOrized", data=account_data, header=self.headers)
-        if res.status_code == 200:
-            account = res.json()
-            my_account = LoginViewModel(**account)
-            return my_account
-        else:
-            return None 
+        self._url = url
+        self._headers = headers
+        self._session = requests.session()
+        self._session.headers.update(self._headers)
 
-    def post_account_generatetoken(self, account:TokenViewModel):
-        """ post account
-     :returns: TokenViewModel
-     """
-        account_data = account.toJson()
-        res = self.session.post(url=f"{self.url}/account/v1/GenerateToken", data=account_data)
-        if res.status_code == 200:
-            account = res.json()
-            my_account = TokenViewModel(**account)
-            return my_account
-        else:
-            return None 
-
-    def post_account_user(self, account:TokenViewModel):
-        """ post account
-     :returns: TokenViewModel
-     """
-        account_data = account.toJson()
-        res = self.session.post(url=f"{self.url}/account/v1/user", data=account_data)
-        if res.status_code == 200:
-            account = res.json()
-            my_account = TokenViewModel(**account)
-            return my_account
-        else:
-            return None 
     
-    def delete_account_by_id(self,id:str):
-        """ delete account
-     :returns: status_code
-     """
-        res = self.session.delete(url=f"{self.url}/account/v1/user/{id}")
-        return res.status_code
+   
+    def post_account_authorize(self, account:LoginViewModel)-> bool:
+        """ post account and authorize
+        :param: account -> TokenViewModel
+        :returns: bool
+         """
+        account_data = account.toJson()
+        res = self._session.post(url=f"{self._url}Authorized", data=account_data, headers=self._headers)
+        if res.status_code == 200:
+         return res.json()
+        else:
+            return None
+   
+
+    def post_account_generatetoken(self, account:LoginViewModel)-> TokenViewModel:
+        """ 
+         post account and generates token
+         :param: account -> TokenViewModel
+         :returns: TokenViewModel
+        """
+  
+        account_data = account.toJson()
+
+        res = self._session.post(url=f"{self._url}GenerateToken", data=account_data, headers=self._headers)
+        if res.status_code == 200:
+         my_token = res.json()["token"]
+         self._session.headers.update(self._headers)
+         self._session.headers.update({'Authorization': f'Bearer {my_token}'})
+         account = TokenViewModel(**res.json())
+         return account
+        else:
+            return None
+          
+
+      
+
+    def post_account_user(self, account:TokenViewModel)->CreateUserResult:
+        """ 
+        post new account
+        :param: account -> TokenViewModel
+        :returns: CreateUserResult
+        """
+        account_data = account.toJson()
+        res = self._session.post(url=f"{self._url}User", data=account_data, headers=self._headers)
+        if res.status_code == 201:
+         account = CreateUserResult(**res.json())
+         return account
+        else:
+            return None
+    
+    def delete_account_by_id(self,id:str)->MessageModal:
+        """ delete account by id
+        :param:id ->str
+        :returns: status_code
+         """
+        res = self._session.delete(url=f"{self._url}User/{id}", headers=self._headers)
+        if res.status_code == 200:
+            user = res.json()
+            my_user = MessageModal(**user)
+            print(my_user)
+            return my_user 
+        else:
+            return None
             
 
-    def get_account_by_id(self,id:str):
-        """ get account
-     :returns: status_code
+    def get_account_by_id(self,id:str)->GetUserResult:
+        """ get account by id
+        :param:id ->str
+        :returns: status_code
         """
-        res = self.session.get(url=f"{self.url}/account/v1/User/{id}")
+        res = self._session.get(url=f"{self._url}/User/{id}")
         if res.status_code == 200:
             user = res.json()
             my_user = GetUserResult(**user)
             return my_user 
         else:
             return None
-
-def main():
-
- session = bearer_auth_session()
- response = session.get('https://bookstore.toolsqa.com/Account/v1/User/c9cca7dc-e9e0-4eed-8d09-d3a08759c544')
- print(response)
- url = "https://bookstore.toolsqa.com/swagger/"
-
-
-if __name__ == "__main__":
-    main()
-
